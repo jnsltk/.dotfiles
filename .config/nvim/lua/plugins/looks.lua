6c05879defcaf5ae15f45d7407d4e314db4a6ec5
jnsltk <janos.litkei@gmail.com>
2023-12-10 10:47:13 +0100

    Updates

--- .config/nvim/lua/plugins/looks.lua
+++ .config/nvim/lua/plugins/looks.lua
@@ -15,7 +15,9 @@ return {
         "rcarriga/nvim-notify",
         opts = {},
         config = function()
-            vim.notify = require("notify")
+            vim.notify = require("notify").setup({
+                background_colour = "#000000"
+            })
         end,
     },
     {
@@ -240,5 +242,45 @@ return {
                 },
             })
         end
-    }
+    },
+    {
+        "folke/noice.nvim",
+        config = function()
+            require("noice").setup({
+                -- add any options here
+                lsp = {
+                    hover = {
+                        enabled = false
+                    },
+                    signature = {
+                        enabled = false
+                    }
+                },
+                routes = {
+                    {
+                        filter = {
+                            event = 'msg_show',
+                            any = {
+                                { find = '%d+L, %d+B' },
+                                { find = '; after #%d+' },
+                                { find = '; before #%d+' },
+                                { find = '%d fewer lines' },
+                                { find = '%d more lines' },
+                            },
+                        },
+                        opts = { skip = true },
+                    }
+                },
+            })
+        end,
+        dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify",
+        }
+    },
 }
+
--- .zshrc
+++ .zshrc
@@ -37,3 +37,19 @@ source $(brew --prefix nvm)/nvm.sh
 
 # STARSHIP
 eval "$(starship init zsh)"
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/Users/jnsltk/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/Users/jnsltk/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/Users/jnsltk/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/Users/jnsltk/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
