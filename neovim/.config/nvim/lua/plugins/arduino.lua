d374c8fbb4b538f4ee818125f9f1feb25825f286
jnsltk <janos.litkei@gmail.com>
2024-02-24 14:52:52 +0100

    changes to yabai

--- neovim/.config/nvim/lua/plugins/arduino.lua
+++ neovim/.config/nvim/lua/plugins/arduino.lua
@@ -1,6 +1,17 @@
 return {
     {
         'stevearc/vim-arduino',
+        cmd = {
+			"ArduinoAttach",
+			"ArduinoChooseBoard",
+			"ArduinoChooseProgrammer",
+			"ArduinoChoosePort",
+			"ArduinoVerify",
+			"ArduinoUpload",
+			"ArduinoSerial",
+			"ArduinoUploadAndSerial",
+			"ArduinoInfo",
+		},
         init = function ()
             vim.keymap.set("n", "<Leader>aa", "<CMD>ArduinoAttach<CR>", silent)
             vim.keymap.set("n", "<Leader>av", "<CMD>ArduinoVerify<CR>", silent)
